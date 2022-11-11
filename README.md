9a2d4855845048067d4c2e9cfa54fac3c7fcb8bc
clinton <clinton@orion.local>
2022-11-11 15:17:04 +0300

    updated the readme with steps on how to run the docker container.

--- README.md
+++ README.md
@@ -2,7 +2,28 @@
 * The idea is to have a logistic regression run in a container and save the model back to the attached folder.   
 * When running the container a volume needs to be attached which contains the csv files and the data schema.    
 
-Note: for now the model is fixed to the spam data but I'll have it work with any data.  
+To try this locally on your pc see the steps below: 
+
+1. Your data folder   
+Note: ensure you name it : *data*      
+should have your files with the following pattern     
+```
+*_train.csv
+*_test.csv
+*_schema.json 
+
+```
+2. spin the docker container   
+
+note: 
+ * logistic is the docker image name    
+ * ensure port 3838 is not in use   
+ * this path _/home/modellingLogistic/data_ should be as it for that's how its predefined inside the container.   
+ 
+```
+docker run -it --rm -p 3838:3838 -v "/path/to/your/data":"/home/modellingLogistic/data"  logistic
+
+```
 
 Next I want to create an API for prediction/inference then see how to link the two (container and API) perhaps they could all run in a container.   
 
--- train.R
+++ train.R
@@ -4,15 +4,15 @@ library(data.table)
 library(readr)
 
 # use pattern to read data : this is to make the model generic 
-fname_train <- dir(path = "./data_try/", pattern = "\\_train.csv$")
-fname_schema <- dir(path = "./data_try/", pattern = "\\_schema.json$")
+fname_train <- dir(path = "./data/", pattern = "\\_train.csv$")
+fname_schema <- dir(path = "./data/", pattern = "\\_schema.json$")
 
 #genericdata <- fread("./data/spam_train.csv")
-genericdata <- fread(paste0("./data_try/",fname_train))
+genericdata <- fread(paste0("./data/",fname_train))
 
 # get the response variable from the schema
 #dataschema <- fromJSON(file = "./data/spam_schema.json")
-dataschema <- fromJSON(file = paste0("./data_try/",fname_schema))
+dataschema <- fromJSON(file = paste0("./data/",fname_schema))
 
 # get the response variable and store it as a character 
 varr <- dataschema$inputDatasets$binaryClassificationBaseMainInput$targetField
@@ -24,7 +24,7 @@ names(genericdata) <- gsub("%","x",names(genericdata))
 # get the field name 
 idfieldname <- dataschema$inputDatasets$binaryClassificationBaseMainInput$idField
 idfieldname <- as.symbol(idfieldname)
-genericdata <- genericdata %>% dplyr::select(-idfieldname)
+genericdata <- genericdata %>% dplyr::select(-all_of(idfieldname))
 
 # function to run the model  
 lets_train <- function(dat,rvar){
@@ -35,7 +35,7 @@ lets_train <- function(dat,rvar){
   theModel <- glm(reformulate(termlabels = indvars, response = rvar),family=binomial(link='logit'), data = dat)
   output.model <- summary(theModel)
   
-  saveRDS(theModel, "model.rds")
+  saveRDS(theModel, "./data/model.rds")
 
 }
 
