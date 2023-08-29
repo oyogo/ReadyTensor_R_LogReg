e1a27316fabc847162e0683efc08cd8c1108d9f9
oyogo <oyogoc@gmail.com>
2023-08-29 08:03:32 +0300

    eval variable

--- ModellingLogistic/preprocessor.R
+++ ModellingLogistic/preprocessor.R
@@ -70,7 +70,7 @@ for (coll in v){
 #genericdata <- data.table(genericdata)
 
   #isTRUE(genericdata[,coll]) && 
-if(isTRUE(genericdata[,coll]) && genericdata[,coll] >= 0){
+if(isTRUE(genericdata[,eval(coll)]) && genericdata[,eval(coll)] >= 0){
   
  # genericdata <-  genericdata[, (coll) := lapply(coll, function(x) {
  #    x <- get(x)
