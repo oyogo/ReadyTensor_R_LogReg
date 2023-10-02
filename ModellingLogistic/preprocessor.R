108f3a649aa5a64a31a9299f7b6e47939be57bf0
oyogo <oyogoc@gmail.com>
2023-10-02 18:35:41 +0300

    restoring encoder back to predict script.

--- ModellingLogistic/preprocessor.R
+++ ModellingLogistic/preprocessor.R
@@ -13,10 +13,6 @@ MODEL_INPUTS_OUTPUTS <- file.path(ROOT_DIR, 'model_inputs_outputs')
 MODEL_ARTIFACTS_PATH <- file.path(MODEL_INPUTS_OUTPUTS, "model", "artifacts")
 IMPUTATION_FILE <- file.path(MODEL_ARTIFACTS_PATH, 'imputation.rds')
 OHE_ENCODER_FILE <- file.path(MODEL_ARTIFACTS_PATH, 'ohe.rds')
-TOP_3_CATEGORIES_MAP <- file.path(MODEL_ARTIFACTS_PATH, "top_3_map.rds")
-LABEL_ENCODER_FILE <- file.path(MODEL_ARTIFACTS_PATH, 'label_encoder.rds')
-ENCODED_TARGET_FILE <- file.path(MODEL_ARTIFACTS_PATH, "encoded_target.rds")
-TEST_ID <- file.path(MODEL_ARTIFACTS_PATH, 'test_id.rds')
 
 preprocessing <- function(df.tn,df.test){ 
   if(df.test==0){
@@ -69,46 +65,16 @@ preprocessing <- function(df.tn,df.test){
     saveRDS(encoded_target, ENCODED_TARGET_FILE)
     
     return(df)
-    
   } else{
     
     imputation_values <- readRDS(IMPUTATION_FILE)
     for (column in names(df.test)[sapply(df.test, function(col) any(is.na(col)))]) {
       df.test[, column][is.na(df.test[, column])] <- imputation_values[[column]]
     }
-    
-    # Saving the id column in a different variable and then dropping it.
-    ids <- df.test[[id_feature]]
-    saveRDS(ids,TEST_ID)
-    df.test[[id_feature]] <- NULL
-    
-    # Encoding
-    # We encode the data using the same encoder that we saved during training.
-    if (length(categorical_features) > 0 && file.exists(OHE_ENCODER_FILE)) {
-      top_3_map <- readRDS(TOP_3_CATEGORIES_MAP)
-      encoder <- readRDS(OHE_ENCODER_FILE)
-      for(col in categorical_features) {
-        # Use the saved top 3 categories to replace values outside these categories with 'Other'
-        df.test[[col]][!(df.test[[col]] %in% top_3_map[[col]])] <- "Other"
-      }
-      
-      test_df_encoded <- dummy_cols(df.test, select_columns = categorical_features, remove_selected_columns = TRUE)
-      encoded_columns <- readRDS(OHE_ENCODER_FILE)
-      # Add missing columns with 0s
-      for (col in encoded_columns) {
-        if (!col %in% colnames(test_df_encoded)) {
-          test_df_encoded[[col]] <- 0
-        }
-      }
-      
-      # Remove extra columns
-      extra_cols <- setdiff(colnames(test_df_encoded), c(colnames(df.test), encoded_columns))
-      df.test <- test_df_encoded[, !names(test_df_encoded) %in% extra_cols]
-    }
-    
     return(df.test)
     
   }
   
 }
 
+#head(preprocessing(genericdata = genericdata, dataschema = dataschema)[[1]])
